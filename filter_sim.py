990e75de57e2ba19c3945bb57f6a964b4b567686
WilliamC Eustis <willceusits@gmail.com>
2020-07-24 21:13:16 -0400

    Hierarchical clustering, many more examples

--- filter_sim.py
+++ filter_sim.py
@@ -1,59 +1,54 @@
 import torchvision.models as models
-import numpy as np
-from itertools import product
 import matplotlib.pyplot as plt
 import math
 from tqdm import tqdm
 import torch
+from util import *
+
+
+linSort = False
+hierSort = True
+vis = False
+randMat = False
+method = "randomMatrixGeneration" if randMat else "randomNumberPermutation"
 
 vgg = models.vgg16(pretrained=True)
-
-def create_permutations(choices, slots):
-    return np.array(list(product(choices, repeat=slots)))
-
 layer_1 = vgg.features[0].weight.data.numpy() # 64, 3, 3, 3
 
+# For each layer, get the filters
 filters = []
 for layer in tqdm([0]): #, 2, 5, 7, 10, 12, 14, 17, 19, 21, 24, 26, 28], desc='Getting Layer Filters'): # Conv2D layers
     for i in range(vgg.features[layer].out_channels): # filters
         for j in range(3): # rgb
             filters.append(vgg.features[layer].weight.data.numpy()[i, j, :, :]) # out, in, w, h (or h, w but who cares)
 print("Filters got")
-choices = np.random.randint(-10, 10, 3)
-perms = create_permutations(choices, 9)
-print(f"Created {len(perms)} permutations")
+
+
+# Create permutations of 3x3 matrices
+#choices = [-1, 0, .5, 2]
+#frames = create_permutations(choices, 9)
+frames = []
+if method == "randomNumberPermutation":
+    frames = create_permutations([0, 1/3, 2/3, 1], 9)
+elif method == "randomMatrixGeneration":
+    frames = np.random.rand(50000, 9)
+else:
+    frames = create_permutations([0, 1 / 3, 2 / 3, 1], 9)
+print(f"Created {len(frames)} permutations")
+
+# Encode the filters by 'convolving' over each frame
 encodings = []
 filters = [np.reshape(filter, 9) for filter in filters]
-
-#filters = [torch.tensor(x).cuda() for x in filters]
-#perms = [torch.tensor(x).cuda().float() for x in perms]
-
 for filter in tqdm(filters, desc='Encoding Filters'):
     encoding = []
-    for perm in perms:
+    for perm in frames:
        encoding.append(np.dot(filter, perm))
     norm = math.sqrt(np.dot(encoding, encoding))
     encodings.append([x/norm for x in encoding])
-'''
-data = torch.zeros([len(encodings), len(encodings)], dtype=torch.float32).cuda()
-encodings = torch.stack([torch.tensor(x).cuda() for x in encodings]).cuda()
 
-
-for i in tqdm(range(len(encodings)), desc='Comparing Encodings'):
-    for j in range(len(encodings[0])):
-        if i<=j:
-            data[i][j] = torch.dot(encodings[i], encodings[j])
-        else:
-            pass
-for i in tqdm(range(len(encodings)), desc='Finalizing Encodings'):
-    for j in range(len(encodings[0])):
-        if i>j:
-            data[i][j] = data[j][i]
-'''
+# Now compare each encoding by dot producting, keep in mind commutivity
 data = []
 encodings = torch.stack([torch.tensor(x).cuda() for x in encodings]).cuda()
-
-
 for i in tqdm(range(len(encodings)), desc='Comparing Encodings'):
     xarr = [0]*len(encodings)
     for j in range(len(encodings)):
@@ -67,15 +62,41 @@ for i in tqdm(range(len(encodings)), desc='Finalizing Encodings'):
         if i>j:
             data[i][j] = data[j][i]
 
-sorted_col_indices = sorted(range(len([row[0] for row in data])), key=lambda k: [row[0] for row in data][k], reverse=True)
-data2 = []
-for row in data:
-    data2.append([row[i] for i in sorted_col_indices])
-data2=[data2[i] for i in sorted_col_indices]
 
-figure = plt.figure()
-axes = figure.add_subplot(111)
-caxes = axes.matshow(np.array(data2), interpolation ='none')
+if linSort:
+    sorted_col_indices = sorted(range(len([row[0] for row in data])), key=lambda k: [row[0] for row in data][k], reverse=True)
+    data2 = []
+    for row in data:
+        data2.append([row[i] for i in sorted_col_indices])
+    data3=[data2[i] for i in sorted_col_indices]
 
-figure.colorbar(caxes)
-figure.savefig(str(choices)+".png")
+if hierSort:
+    dist_mat = []
+    for row in data:
+        dst_row = []
+        for i in row:
+            dst_row.append(scaled_abs_x(i))
+        dist_mat.append(dst_row)
+
+    for i in range(len(dist_mat)):
+        assert abs(dist_mat[i][i]) < epsilon
+        dist_mat[i][i] = 0
+    # https://stats.stackexchange.com/questions/195456/how-to-select-a-clustering-method-how-to-validate-a-cluster-solution-to-warran/195481#195481
+    # https://stats.stackexchange.com/questions/195446/choosing-the-right-linkage-method-for-hierarchical-clustering
+    methods = ["single", 'complete', 'average', 'weighted', 'centroid', 'median', 'ward']
+    for method in methods:
+        print("Method:\t", method)
+
+        ordered_dist_mat, res_order, res_linkage = compute_serial_matrix(dist_mat, method)
+
+        plt.pcolormesh(ordered_dist_mat)
+        plt.xlim([0, len(dist_mat)])
+        plt.ylim([0, len(dist_mat)])
+        plt.show(block=False)
+        plt.savefig("permutation_mag1_noNeg_thirds/"+str(method)+".png")
+
+if vis:
+    figure = plt.figure()
+    axes = figure.add_subplot(111)
+    caxes = axes.matshow(np.array(data3), interpolation ='none')
+    figure.colorbar(caxes)
---
+++ util.py
@@ -0,0 +1,61 @@
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+from fastcluster import linkage
+from itertools import product
+
+def seriation(Z, N, cur_index):
+    '''
+        input:
+            - Z is a hierarchical tree (dendrogram)
+            - N is the number of points given to the clustering process
+            - cur_index is the position in the tree for the recursive traversal
+        output:
+            - order implied by the hierarchical tree Z
+
+        seriation computes the order implied by a hierarchical tree (dendrogram)
+    '''
+    if cur_index < N:
+        return [cur_index]
+    else:
+        left = int(Z[cur_index - N, 0])
+        right = int(Z[cur_index - N, 1])
+        return (seriation(Z, N, left) + seriation(Z, N, right))
+
+
+def compute_serial_matrix(dist_mat, method="ward"):
+    '''
+        input:
+            - dist_mat is a distance matrix
+            - method = ["ward","single","average","complete"]
+        output:
+            - seriated_dist is the input dist_mat,
+              but with re-ordered rows and columns
+              according to the seriation, i.e. the
+              order implied by the hierarchical tree
+            - res_order is the order implied by
+              the hierarhical tree
+            - res_linkage is the hierarhical tree (dendrogram)
+
+        compute_serial_matrix transforms a distance matrix into
+        a sorted distance matrix according to the order implied
+        by the hierarchical tree (dendrogram)
+    '''
+    N = len(dist_mat)
+    flat_dist_mat = squareform(dist_mat)
+    res_linkage = linkage(flat_dist_mat, method=method, preserve_input=True)
+    res_order = seriation(res_linkage, N, N + N - 2)
+    print(len(res_order))
+    seriated_dist = []
+    for row in dist_mat:
+        seriated_dist.append([row[i] for i in res_order])
+    seriated_dist=[seriated_dist[i] for i in res_order]
+    return seriated_dist, res_order, res_linkage
+
+
+def scaled_abs_x(x):
+    return -1 * abs(x) + 1
+
+def create_permutations(choices, slots):
+    return np.array(list(product(choices, repeat=slots)))
+
+epsilon = .00001
